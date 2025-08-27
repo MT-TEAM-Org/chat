18af6bdc6fa64ade4219b1e9b44fdc8addc7b867
dong3058 <hwangdonggeun70@gmail.com>
2025-08-27 21:40:22 +0900

    fix table anme

--- src/main/java/com/myteam/chat/kafka/cosumeserver/domain/Chat.java
+++ src/main/java/com/myteam/chat/kafka/cosumeserver/domain/Chat.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 @Entity
 @Getter
 @NoArgsConstructor
-@Table(name = "chat")
+//@Table(name = "chat", schema = "playhive")
 public class Chat extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
